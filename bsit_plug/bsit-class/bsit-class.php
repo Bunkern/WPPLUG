8cb69db419eed41fdd7ec7e8d2db62f29ee8b3e9
Bunkern <kenneth@bunkern.dk>
2019-11-19 12:31:39 +0100

    BSIT-Wiget updated

--- bsit_plug/bsit-class/bsit-class.php
+++ bsit_plug/bsit-class/bsit-class.php
@@ -29,7 +29,7 @@ class BSIT_widget extends WP_Widget {
 		if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
-		echo '<div class="g-ytsubscribe" data-channel="GoogleDevelopers" data-layout="default" data-count="default">jjj</div>';
+		echo '<div class="g-ytsubscribe" data-channel="'.$instance['channel'].'" data-layout="'.$instance['layout'].'" data-count="'.$instance['count'].'"></div>';
 		echo $args['after_widget'];
 	}
 
@@ -42,10 +42,31 @@ class BSIT_widget extends WP_Widget {
 	 */
 	public function form( $instance ) {
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'BSIT Widget!!', 'bsit_domain' );
+        $channel = ! empty( $instance['channel'] ) ? $instance['channel'] : esc_html__( 'GoogleDevelopers', 'bsit_domain' );
+        $layout = ! empty( $instance['layout'] ) ? $instance['layout'] : esc_html__( 'default', 'bsit_domain' );
+        $count = ! empty( $instance['count'] ) ? $instance['count'] : esc_html__( 'default', 'bsit_domain' );
 		?>
 		<p>
 		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'bsit_domain' ); ?></label> 
 		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+        <p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>"><?php esc_attr_e( 'Channel:', 'bsit_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'channel' ) ); ?>" type="text" value="<?php echo esc_attr( $channel ); ?>">
+		</p>
+        <p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>"><?php esc_attr_e( 'Layout:', 'bsit_domain' ); ?></label> 
+		<select class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'layout' ) ); ?>">
+        <option value="default" <?php echo ($layout == 'default') ? 'selected' : ''; ?>> Default </option>
+        <option value="full" <?php echo ($layout == 'full') ? 'selected' : ''; ?>> Full </option>
+        </select>
+		</p>
+        <p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>"><?php esc_attr_e( 'Count:', 'bsit_domain' ); ?></label> 
+		<select class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'count' ) ); ?>">
+        <option value="default" <?php echo ($count == 'default') ? 'selected' : ''; ?>> Default </option>
+        <option value="hidden" <?php echo ($count == 'hidden') ? 'selected' : ''; ?>> Hidden </option>
+        </select>
 		</p>
 		<?php 
 	}
@@ -63,6 +84,9 @@ class BSIT_widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+        $instance['channel'] = ( ! empty( $new_instance['channel'] ) ) ? sanitize_text_field( $new_instance['channel'] ) : '';
+        $instance['layout'] = ( ! empty( $new_instance['layout'] ) ) ? sanitize_text_field( $new_instance['layout'] ) : '';
+        $instance['count'] = ( ! empty( $new_instance['count'] ) ) ? sanitize_text_field( $new_instance['count'] ) : '';
 
 		return $instance;
 	}
